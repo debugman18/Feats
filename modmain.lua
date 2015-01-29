a15619958f1421ee168c5008249678c8eaee0a8d
debugman18 <thebandforums@yahoo.com>
2015-01-29 15:27:29 -0600

    Saving and loading is now functional.

--- modmain.lua
+++ modmain.lua
@@ -40,6 +40,65 @@ local function patch_feats(self)
     -- We don't want to abandon the old set function.
     self.old_Set = self.Set
 
+    -- Avoid redundancy and errors.
+    function CreateFeats()
+        print("Checking for persistdata.")
+
+        -- TODO: Loaded feats doesn't exist yet.
+        print("STEP-2: " .. tostring(loaded_feats))
+
+
+        if self.persistdata then
+            if not loaded_feats then
+                print("Feats will now persist.")
+                self.persistdata.feats = {}
+                self.dirty = true
+                self:Save()
+            else
+                print("Feats already persist.")
+                self.persistdata.feats = loaded_feats
+                for k,v in pairs(self.persistdata.feats) do
+                    print("DEBUGTABLE")
+                    print(tostring(k))
+                    for x,y in pairs(v) do
+                        print(tostring(x))
+                        print(tostring(y))
+                    end
+                end
+            end
+        end 
+    end
+
+    -- Add a feat to the achievement list.
+    function AddFeat(name, description, locked, hidden)
+        print("Attempting to add feat(s).")
+
+        -- Add values straight to this temp table.
+        local temp_key = {
+            name = name, 
+            description = description, 
+            locked = locked or nil, 
+            hidden = hidden or nil
+        }
+
+        if self.persistdata and self.persistdata.feats then
+            for key,feat in pairs(self.persistdata.feats) do
+                for n,string in pairs(feat) do
+                    if n == name then
+                        -- Add our table to the feats table.
+                        print("Added " .. name .. " to feats list.")
+                        table.insert(self.persistdata.feats, temp_key)
+                        self.dirty = true
+                        self:Save() 
+                    else
+                        -- The key already exists.
+                        print("The key (" .. name .. ") already exists.")
+                    end
+                end
+            end
+        end
+    end
+
     -- Load our dirty variable.
     self.Set = function(self, str, callback)
         print("DEBUGSTRING: " .. str)
@@ -54,71 +113,18 @@ local function patch_feats(self)
                 end
             end
         end
-        self.persistdata.feats = data["feats"]
+        loaded_feats = data["feats"]
+        print("STEP-1: " .. tostring(loaded_feats))
+
+        -- Initialize the feats table.
+        CreateFeats()
+
+        -- This is a sample feat for testing.
+        AddFeat("Sample Feat", "Sample Feat Description")
 
         self:old_Set(str, callback)
-    end   
+    end 
 
-    -- Avoid redundancy and errors.
-    function CreateFeats()
-        print("Checking for persistdata.")
-        if self.persistdata then
-            if not loaded_feats then
-                print("Feats will now persist.")
-                self.persistdata.feats = {debug = {"debug_name", "debug_description"}}
-                self.dirty = true
-                self:Save()
-            else
-                print("Feats already persist.")
-            end
-        end 
-    end
-
-    -- Add a feat to the achievement list.
-    function AddFeat(name, description, locked, hidden)
-        print("Attempting to add feat.")
-        print(name)
-        print(description)
-        -- Add values straight to this temp table.
-        local temp_key = {
-            name = name, 
-            description = description, 
-            locked = locked or nil, 
-            hidden = hidden or nil
-        }
-
-        if self.persistdata then
-            if not self.persistdata.feats[temp_key] then
-                -- Add our table to the feats table.
-                if self.persistdata.feats then
-                    print("Added " .. name .. " to feats list.")
-                    table.insert(self.persistdata.feats, temp_key)
-                    self.dirty = true
-                    self:Save()                    
-                end
-            else
-                -- The key already exists.
-                print("The key (" .. name .. ") already exists.")
-            end
-        end
-    end
-
-    -- Print list of feats.
-    for k,v in pairs(self.persistdata) do
-        print(k)
-        if type(v) == "table" then
-            print(v)
-            for a,b in pairs(v) do
-                print(b)
-            end
-        end
-    end
-
-    -- Initialize the feats table.
-    CreateFeats()
-
-    -- This is a sample feat for testing.
-    AddFeat("Sample Feat", "Sample Feat Description")
 end
 
 AddGlobalClassPostConstruct("playerprofile", "PlayerProfile", patch_feats)
