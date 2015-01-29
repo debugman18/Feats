bf7a5b7b55a84b3526460be67a54dfda32d1409e
debugman18 <thebandforums@yahoo.com>
2015-01-28 20:27:30 -0600

    Began implmentation.

--- modmain.lua
+++ modmain.lua
@@ -1,15 +1,89 @@
--- Adds the button for feats.
-local function append_feats(self)
+-- For convenience purposes.
+local require = GLOBAL.require
 
-    -- Ease of use.
-    local Image = require "widgets/image"
-    local ImageButton = require "widgets/imagebutton"
-    local Text = require "widgets/text"	
-    local UIAnim = require "widgets/uianim"
+require "prefabutil"
 
-    -- Wrap it up.
-    self:MainMenu()
-    self.menu:SetFocus()
+local Screen = require "widgets/screen"
+local Menu = require "widgets/menu"
+
+local FeatsScreen = require "screens/featsscreen"
+
+local achievements = require "achievements"
+
+----------------------------------------------------------------------------
+
+-- Opens the feats screen.
+local function FeatsOpen()
+    --TheFrontEnd:PushScreen(FeatsScreen())
 end
 
-TheMod:AddClassPostConstruct("screens/mainscreen", append_feats)
+-- Adds the button for feats.
+local function append_feats(self)
+    local feats_meta = {{ text = "Feats", cb = function() FeatsOpen() end }}
+    self.featsbutton = self.bmenu:AddChild(Menu(feats_meta, 70))
+    self.featsbutton:SetPosition(320, 2, 0)
+    self.featsbutton:SetScale(1)   
+
+    -- Lets make it fit.
+    self.bmenu:SetPosition(-120, -250, 0)
+end
+
+AddGlobalClassPostConstruct("screens/loadgamescreen", "LoadGameScreen", append_feats)
+
+----------------------------------------------------------------------------
+
+-- Patch our feats into profile data.
+local function patch_feats(self)
+
+    -- Avoid redundancy and errors.
+    if self.persistdata then
+        if not self.persistdata.feats then
+            print("Feats will now persist.")
+            self.persistdata.feats = {"debug"}
+            self:Save()
+        else
+            print("Feats already persist.")
+        end
+    end
+
+    -- Add a feat to the achievement list.
+    local function AddFeat(profile, key, name, description, locked, hidden)
+        if not self.persistdata.feats[key] then
+            print("Added " .. key .. " to feats list.")
+            for k,v in pairs(self.persistdata) do
+                if k == feats then
+                    for a,b in pairs(v) do
+                        if b then
+                            -- The key already exists.
+                            print("The key (" .. key .. ") already exists.")
+                        else
+                            -- The key needs to be added.
+                            table.insert(a, key)
+                            --self.persistdata.feats[key].name = name
+                            --self.persistdata.feats[key].description = description
+                            --self.persistdata.feats[key].locked = locked or nil
+                            --self.persistdata.feats[key].hidden = hidden or nil 
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    -- Print list of feats.
+    print("Checking for persistdata.")
+    for k,v in pairs(self.persistdata) do
+        print(k)
+        if type(v) == "table" then
+            print(v)
+            for a,b in pairs(v) do
+                print("Value2" .. b)
+            end
+        end
+    end
+
+    -- This is a sample feat for testing.
+    AddFeat("sample_feat", "Sample Feat", "Sample Feat Description")
+end
+
+AddGlobalClassPostConstruct("playerprofile", "PlayerProfile", patch_feats)
---
+++ scripts/screens/featsscreen.lua
@@ -0,0 +1,22 @@
+-- For convenience.
+local Screen = require "widgets/screen"
+local Menu = require "widgets/menu"
+local AnimButton = require "widgets/animbutton"
+local ImageButton = require "widgets/imagebutton"
+local Spinner = require "widgets/spinner"
+local Text = require "widgets/text"
+local Image = require "widgets/image"
+local Widget = require "widgets/widget"
+local HoverText = require "widgets/hoverer"
+local NumericSpinner = require "widgets/numericspinner"
+local PopupDialogScreen = require "screens/popupdialog"
+local BigPopupDialogScreen = require "screens/bigpopupdialog"
+local MainScreen = require "screens/mainscreen"
+
+-- Create the feats screen.
+local FeatsScreen = Class(Screen, function(self, profile)
+    Widget._ctor(self, "FeatsScreen")
+    TheFrontEnd:PopScreen(self)
+end)
+
+return FeatsScreen
